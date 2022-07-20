33642fa046d521a6cda031e3078c9b38dd041ab3
oduvan <a.lyabah@checkio.org>
2022-07-20 13:00:39 +0300

    v1

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,59 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [[3, 5, 1, 1, 9]],
+            "answer": 1,
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": [[1,2,3]],
+            "answer": 0,
+        },
+        {
+            "input": [[1,2,3, 4]],
+            "answer": 0,
+        },
+        {
+            "input": [[1,2,3, 4, 5]],
+            "answer": 1,
+        },
+        {
+            "input": [[1, 1, 1, 1]],
+            "answer": 0,
+        },
+        {
+            "input": [[1, 1, 1]],
+            "answer": 1,
+        },
+        {
+            "input": [[1, 10, 1]],
+            "answer": 8,
+        },
+        {
+            "input": [[1, 10, 1, 8]],
+            "answer": 0,
+        },
+        {
+            "input": [[]],
+            "answer": 0,
+        },
+        {
+            "input": [[1]],
+            "answer": 1,
+        },
+        {
+            "input": [[10, 20, 30, 50, 100, 10, 20, 10]],
+            "answer": 10,
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": [[5,3,8,34,45,35,7,54,7,23,5,6,2,3,2,97,5,67,5]],
+            "answer": 1,
         },
+
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": [[4,3,5,5,5,5,5,3,2,5,6,7,8,5,4,6,8,8,4,2,2,2]],
+            "answer": 0,
         }
     ]
 }
